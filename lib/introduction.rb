cd409173b507508a13f48ae914cb2a8ab0d86856
Albert LaQue <albertlaque@gmail.com>
2019-10-20 23:53:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 #introduction
-def greeting_someone_new(Eminem)
+def greeting_someone_new(name)
   puts "Hi, my name is #{name}."
 end
 
