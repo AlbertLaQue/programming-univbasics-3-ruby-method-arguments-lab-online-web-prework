ea3bba5a99c45a18fe601c94fcd8d022d034083a
Albert LaQue <albertlaque@gmail.com>
2019-10-21 00:39:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -17,6 +17,11 @@ end
 
 introduction_with_language("Slim Shady", "Rap")
 
+#introduction_with_language_optional
+def introduction_with_language(name, language)
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
 
 
 
